943b08890b2dfa5a21f9f8027f7fd99926d1f2ef
Danilo Horta <danilo.horta@pm.me>
2022-04-26 00:27:06 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# c-toolbelt
+
+```
+bash -c "$(curl -fsSL https://bit.ly/3Kv06KA)" -s horta/c-toolbelt
+```
---
+++ include/c_toolbelt/c_toolbelt.h
@@ -0,0 +1,25 @@
+#ifndef C_TOOLBELT_H
+#define C_TOOLBELT_H
+
+#include "c_toolbelt/export.h"
+#include <stddef.h>
+
+#define CTB_ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+
+/*
+ * Copy string src to buffer dst of size dsize. At most dsize-1
+ * chars will be copied.  Always NUL terminates (unless dsize == 0).
+ * Returns strlen(src); if retval >= dsize, truncation occurred.
+ */
+CTB_API size_t ctb_strlcpy(char *dst, const char *src, size_t dsize);
+
+#define CTB_STRCPY(ptr, dst, src)                                              \
+    ctb_strcpy((ptr)->dst, src, __CTB_ARRAY_SIZE_OF(*(ptr), dst))
+
+/* ------ PRIVATE MACROS ------ */
+#define __CTB_MEMBER_REF(var, member) ((__typeof__(var) *)0)->member
+
+#define __CTB_ARRAY_SIZE_OF(var, member)                                       \
+    CTB_ARRAY_SIZE(__CTB_MEMBER_REF((var), member))
+
+#endif
