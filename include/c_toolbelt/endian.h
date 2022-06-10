c34a0b4be30c566e953161b3a23bd99a8492a1a4
Danilo Horta <danilo.horta@pm.me>
2022-06-10 11:00:45 +0100

    CBT_API

--- include/c_toolbelt/endian.h
+++ include/c_toolbelt/endian.h
@@ -4,12 +4,12 @@
 #include "c_toolbelt/export.h"
 #include <stdint.h>
 
-uint16_t ctb_htons(uint16_t);
-uint32_t ctb_htonl(uint32_t);
-uint64_t ctb_htonll(uint64_t);
+CTB_API uint16_t ctb_htons(uint16_t);
+CTB_API uint32_t ctb_htonl(uint32_t);
+CTB_API uint64_t ctb_htonll(uint64_t);
 
-uint16_t ctb_ntohs(uint16_t);
-uint32_t ctb_ntohl(uint32_t);
-uint64_t ctb_ntohll(uint64_t);
+CTB_API uint16_t ctb_ntohs(uint16_t);
+CTB_API uint32_t ctb_ntohl(uint32_t);
+CTB_API uint64_t ctb_ntohll(uint64_t);
 
 #endif
