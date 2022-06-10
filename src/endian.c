bb92f6d30ea6f3efa87de6762fd5c951f3bbfdc7
Danilo Horta <danilo.horta@pm.me>
2022-06-10 12:31:22 +0100

    ctb prefix

--- src/endian.c
+++ src/endian.c
@@ -36,10 +36,10 @@ static inline uint64_t ntohll(uint64_t x) { return __builtin_bswap64(x); }
 #endif
 #endif
 
-uint16_t cbt_htons(uint16_t x) { return htons(x); }
-uint32_t cbt_htonl(uint32_t x) { return htonl(x); }
-uint64_t cbt_htonll(uint64_t x) { return htonll(x); }
+uint16_t ctb_htons(uint16_t x) { return htons(x); }
+uint32_t ctb_htonl(uint32_t x) { return htonl(x); }
+uint64_t ctb_htonll(uint64_t x) { return htonll(x); }
 
-uint16_t cbt_ntohs(uint16_t x) { return ntohs(x); }
-uint32_t cbt_ntohl(uint32_t x) { return ntohl(x); }
-uint64_t cbt_ntohll(uint64_t x) { return ntohll(x); }
+uint16_t ctb_ntohs(uint16_t x) { return ntohs(x); }
+uint32_t ctb_ntohl(uint32_t x) { return ntohl(x); }
+uint64_t ctb_ntohll(uint64_t x) { return ntohll(x); }
